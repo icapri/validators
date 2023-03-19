e6b1dbd8caf2c38089b8e512da9bbea2434c41b8
I. Kapri <my@mail.com>
2023-03-19 22:30:55 +0100

    small fix of isArray

--- src/validators/collections/is-array.ts
+++ src/validators/collections/is-array.ts
@@ -4,7 +4,7 @@
  * @param {*} value Contains some value.
  * @return {Boolean} whether the given value is an `Array` object.
  */
-export function isArray(value: any): value is Array<any> {
+export function isArray(value: any): value is any[] | readonly any[] {
   // there are several ways of checking whether an object is an array
   // but I chose for this one because of backwards compatibility
   // alternatives would be `value.constructor === Array` or the
